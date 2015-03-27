56819cf53bd6ef193547c4bf269100fe91225a3c
GarryY <garry.yavnilovich@safenet-inc.com>
2015-03-26 22:06:11 -0700

    linked list, tested

--- ucache-test.py
+++ ucache-test.py
@@ -1,16 +1,44 @@
 #!/usr/bin/python
+import sys
+from ucache import UserStore
 
-from ucache import getUser
+if len(sys.argv) <= 1:
+    print 'No cache size specified'
+    sys.exit(1)
 
-user_test_set1 = {
-                    'user1':True,
-                    'user2':True,
-                    'user3':False,
-                    'admin':True,
-                    }
+cache_size = int(sys.argv[1])
+ustore = UserStore(cache_size)
 
-for u, expected in user_test_set1.items():
-    udata = getUser(u)
-    print u, '=', udata, '/', 'passed' if udata or not expected else 'NOT passed'
+user_test_set0 = (
+                    ('1',{'found':True,'cache':'1'}),
+                    ('2',{'found':False,'cache':'21'}),
+                    ('3',{'found':True,'cache':'321'}),
+                    ('2',{'found':True,'cache':'231'}),
+                    ('1',{'found':True,'cache':'123'}),
+                    ('4',{'found':True,'cache':'412'}),
+                )
 
+user_test_set1 = (
+                    ('1',{'found':True,'cache':'1'}),
+                    ('99',{'found':False,'cache':'1'}),
+                    ('2',{'found':True,'cache':'21'}),
+                    ('2',{'found':True,'cache':'21'}),
+                    ('1',{'found':True,'cache':'12'}),
+                    ('5',{'found':True,'cache':'512'}),
+                    ('1',{'found':True,'cache':'152'}),
+                    ('6',{'found':True,'cache':'615'}),
+                    ('6',{'found':True,'cache':'615'}),
+                    ('99',{'found':False,'cache':'615'}),
+                )
 
+def test(test_set):
+    for user_id, expected in test_set:
+        udata = ustore.getUser(user_id)
+        cache = '%r' % ustore._ucache
+        passed = (udata or not expected['found']) and expected['cache'] == cache
+        print user_id, ':', '' if passed else 'NOT', 'passed | user data:', udata
+        print ustore._ucache
+        print '***'
+
+test(user_test_set0)
+#test(user_test_set1)
--- ucache.py
+++ ucache.py
@@ -1,42 +1,120 @@
 #!/usr/bin/python
 
+class CacheNode:
+    def __init__(self, key=None, data=None):
+        self.key = key
+        self.data = data
+        self._next = None
+        self._prev = None
+    def __str__(self):
+        return '%s' % self.key
+    def __repr__(self):
+        return '(%s,%s)' % (self.key,self.data)
+
+def listiter(node):
+    while node:
+        yield node
+        node = node._next
+
 class UserCache:
-    def __init__(self):
-        pass
-    def get(self, uname):
+    '''
+    User cache implemented as doubly linked list
+    '''
+    def __init__(self, cache_limit):
+        self._cache_length = 0
+        self._cache_head = None
+        self._cache_tail = None
+        self._cache_limit = cache_limit
+
+    def _update_head(self, node):
+        if node == self._cache_head:
+            return
+        if node._next:
+            node._next._prev = node._prev
+        else:
+            self._cache_tail = node._prev
+        if node._prev:
+            node._prev._next = node._next
+        node._next = self._cache_head
+        node._next._prev = node
+        self._cache_head = node
+
+    def find(self, key):
+        '''
+        Searches the cache for the key, updates list the list head if found
+        :param key:
+        :return:node data if found, else None
+        '''
+        for node in listiter(self._cache_head):
+            if node.key == key:
+                self._update_head(node)
+                return node.data
         return None
-    def update(self, uname, udata):
-        pass
+
+    def _is_full(self):
+        return self._cache_length >= self._cache_limit
+
+    def add(self, key, data):
+        node = CacheNode(key=key,data=data)
+        node._next = self._cache_head
+        if self._cache_head:
+            self._cache_head._prev = node
+        self._cache_head = node
+
+        if not self._is_full():
+            if self._cache_length == 1:
+                self._cache_tail = self._cache_head
+            self._cache_length += 1
+        else:
+            tail = self._cache_tail
+            self._cache_tail = self._cache_tail._prev
+            self._cache_tail._next = None
+            del tail
+
+    def __str__(self):
+        s = 'cache: l=%d (%s) | ' % (self._cache_length,'full' if self._is_full() else 'not full')
+        for node in listiter(self._cache_head):
+            s = s + str(node) + ' '
+        return s
+
+    def __repr__(self):
+        s = ''
+        for node in listiter(self._cache_head):
+            s += str(node)
+        return s
 
 class UserStore:
 
-    def __init__(self, dbname='./ucache.dat'):
+    def __init__(self, cache_limit, dbname='./ucache.dat'):
         self._dbname = dbname
-        self._ucache = UserCache()
+        self._ucache = UserCache(cache_limit)
         self._userdb = {}
         with open(self._dbname) as f:
             for l in f:
-                user, data = l.split('=')
-                self._userdb[user] = { v.split(':')[0].strip():v.split(':')[1].strip() for v in data.split(',') }
+                user_id, data = l.split('=')
+                self._userdb[int(user_id)] = { v.split(':')[0].strip():v.split(':')[1].strip() for v in data.split(',') }
 
-    def getUser(self, uname):
-        udata = self._ucache.get(uname)
+    def getUser(self, user_id):
+        '''
+        :param user_id: string
+        :return: user data dictionary
+        '''
+        user_id = int(user_id)
+        udata = self._ucache.find(user_id)
         if udata:
             return udata
         try:
-            udata = self._userdb[uname]
-            self._ucache.update(uname, udata)
+            udata = self._userdb[user_id]
+            self._ucache.add(user_id, udata)
             return udata
         except:
             pass
         return None
 
-
-def getUser(uname):
-    return UserStore().getUser(uname)
+################################################
 
 if __name__ == '__main__':
-    ustore = UserStore()
+    ustore = UserStore(3)
     for u, ud in ustore._userdb.items():
         print u, ud
 
